442800ce108baffd4ebe75db91b1fdc2fed3a7e0
rainite <kevinchenlife@gmail.com>
2020-11-23 14:36:03 -0800

    new egg support

---
+++ entry.py
@@ -0,0 +1,6 @@
+from main import Main
+
+main = Main()
+main.start()
+
+
---
+++ main.py
@@ -0,0 +1,84 @@
+import time
+from playsound import playsound
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def playSound():
+    while True:
+        time.sleep(0.2)
+        playsound("alart.wav")
+
+
+class Main:
+    browser = None
+
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+
+    def start(self):
+        self.browser.get("https://www.newegg.com/#")
+        time.sleep(5)
+        try:
+            self.browser.find_element_by_class_name("close").click()
+        except:
+            print("No pop-in window, skip")
+
+        # log in
+        if self.browser.find_element_by_class_name("nav-complex-inner"):
+            link = self.browser.find_element_by_class_name("nav-complex-inner").get_attribute('href')
+            self.browser.get(link)
+            self.browser.find_element(By.ID, "labeled-input-signEmail").send_keys("kevinchenlife@gmail.com")
+            self.clickSignIn()
+
+            time.sleep(2)
+            if self.browser.find_element_by_class_name("alert-body"):
+                secKey = input("Enter email Security Code:")
+                divList = self.browser.find_elements_by_class_name("form-v-code")
+                divList[0].find_element_by_xpath(".//input").send_keys(secKey)
+                self.clickSignIn()
+                time.sleep(1)
+
+        # search
+        # first time search
+        searchBox = self.browser.find_element_by_class_name("header2020-search-box")
+        searchBox.find_element_by_xpath(".//input").send_keys("asus tuf 3080")
+        # loop search
+        findOne = False
+        while not findOne:
+            time.sleep(2)
+            searchButton = self.browser.find_element_by_class_name("header2020-search-button")
+            searchButton.find_element_by_xpath(".//button").click()
+            itemList = self.browser.find_elements_by_class_name("item-cell")
+
+            # loop item
+            time.sleep(1)
+            for item in itemList:
+                try:
+                    button = item.find_element_by_class_name("item-button-area")
+                    buttonText = button.find_element_by_xpath(".//button")
+                    if buttonText.text != "AUTO NOTIFY":
+                        buttonText.click()
+                        findOne = True
+                        break
+                except:
+                    continue
+
+            # find one
+            if findOne:
+                summary = self.browser.find_element_by_class_name("item-actions")
+                cartButtons = summary.find_elements_by_xpath(".//button")
+                time.sleep(1)
+                for button in cartButtons:
+                    if "Checkout".upper() in str(button.text).upper():
+                        button.click()
+                        break
+                playSound()
+
+    def clickSignIn(self):
+        signInList = self.browser.find_elements_by_class_name("form-cell")
+        for element in signInList:
+            if element.text == "SIGN IN":
+                element.click()
+                break
